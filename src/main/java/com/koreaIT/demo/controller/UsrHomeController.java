30304b4d587383575d26a26e84b31cafa52fafbb
jeongo0 <jeongo2023911@gamil.com>
2023-12-01 17:30:36 +0900

    jsp connect, main.jsp

--- src/main/java/com/koreaIT/demo/controller/UsrHomeController.java
+++ src/main/java/com/koreaIT/demo/controller/UsrHomeController.java
@@ -2,28 +2,18 @@ package com.koreaIT.demo.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class UsrHomeController {
-		
-	int a;
-	
-	UsrHomeController () {
-		this.a = 0;
-	}
-	
+
 	@RequestMapping("/usr/home/main")
-	@ResponseBody
 	public String showMain() {
-		return "안녕하세요1";
+		return "usr/home/main";
 	}
 	
-	@RequestMapping("/usr/home/main2")
-	@ResponseBody
-	public int showMain2() {
-		
-		return a++;
+	@RequestMapping("/")
+	public String showRoot() {
+		return "redirect:/usr/home/main";
 	}
 	
 }
