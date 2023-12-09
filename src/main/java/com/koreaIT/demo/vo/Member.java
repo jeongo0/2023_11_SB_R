939d6c48ecb8a8c15c72b1a8a4cac48c96930955
jeongo0 <jeongo2023911@gamil.com>
2023-12-09 17:16:13 +0900

    jsHistoryBack(), jsReplace(), Rq()

--- src/main/java/com/koreaIT/demo/vo/Member.java
+++ src/main/java/com/koreaIT/demo/vo/Member.java
@@ -2,14 +2,16 @@ package com.koreaIT.demo.vo;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @AllArgsConstructor
+@NoArgsConstructor
 public class Member {
 	private int id;
 	private String regDate;
-	private String loginId;
 	private String updateDate;
+	private String loginId;
 	private String loginPw;
 	private int authLevel;
 	private String name;
